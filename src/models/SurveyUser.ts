af324f56df7ef76b6e0e2b6b7cb3ca6613615d7c
guibafica <gb.engcomp@hotmail.com>
2021-03-03 20:29:10 -0300

    CHORE: Finalizes the fourth day

--- src/models/SurveyUser.ts
+++ src/models/SurveyUser.ts
@@ -1,9 +1,12 @@
 /* eslint-disable camelcase */
 /* eslint-disable import/prefer-default-export */
 import {
-  Entity, PrimaryColumn, Column, CreateDateColumn,
+  Entity, PrimaryColumn, Column, CreateDateColumn, ManyToOne, JoinColumn,
 } from 'typeorm';
 import { v4 as uuid } from 'uuid';
+
+import { User } from './User';
+import { Survey } from './Survey';
 
 @Entity('surveys_users')
 class SurveyUser {
@@ -13,8 +16,16 @@ class SurveyUser {
   @Column()
   user_id: string;
 
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
   @Column()
   survey_id: string;
+
+  @ManyToOne(() => Survey)
+  @JoinColumn({ name: 'survey_id' })
+  survey: Survey;
 
   @Column()
   value: string;
